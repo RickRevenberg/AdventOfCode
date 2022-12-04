7801c2058ef559ae11a336df70399de7d47b80a1
Rick Revenberg <Rick.Revenberg@topicus.nl>
2022-12-04 09:34:05 +0100

    day 4

--- AdventOfCode/PuzzleSolvers/2022/Day_04.cs
+++ AdventOfCode/PuzzleSolvers/2022/Day_04.cs
@@ -1,5 +1,8 @@
 ﻿namespace AdventOfCode.PuzzleSolvers._2022
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
 	using System.Threading.Tasks;
 	using NUnit.Framework;
 
@@ -7,20 +10,44 @@
 	{
 		public override int Day => 4;
 
+		private List<List<(int start, int end)>> formattedInput;
+
 		[SetUp]
 		public async Task SetUp()
 		{
-			var what = (await this.GetInput());
+			formattedInput = (await this.GetInput()).Split("\n").Select(x =>
+					x.Split(',').Select(y => (Convert.ToInt32(y.Split('-')[0]), Convert.ToInt32(y.Split('-')[1])))
+						.ToList())
+				.ToList();
 		}
 		
 		[Test]
 		public override void PartOne()
 		{
+			var inclusivePairs = 0;
+
+			foreach (var input in formattedInput)
+			{
+				inclusivePairs +=
+					(input[0].start <= input[1].start && input[0].end >= input[1].end) ||
+					(input[1].start <= input[0].start && input[1].end >= input[0].end)
+						? 1 : 0;
+
+			}
+
+			Assert.Pass(inclusivePairs.ToString());
 		}
 
 		[Test]
 		public override void PartTwo()
 		{
+			var overlappingPairs = 0;
+			foreach (var input in formattedInput)
+			{
+				overlappingPairs += (input[0].end < input[1].start || input[0].start > input[1].end) ? 0 : 1;
+			}
+
+			Assert.Pass(overlappingPairs.ToString());
 		}
 	}
 }
