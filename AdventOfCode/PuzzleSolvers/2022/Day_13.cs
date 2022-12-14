6d67055f5e58f0339c22b4225d97597e7c0344c9
Rick Revenberg <Rick.Revenberg@topicus.nl>
2022-12-14 13:28:43 +0100

    Day 13 & 14

---
+++ AdventOfCode/PuzzleSolvers/2022/Day_13.cs
@@ -0,0 +1,237 @@
+﻿namespace AdventOfCode.PuzzleSolvers._2022
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text.RegularExpressions;
+	using System.Threading.Tasks;
+	using Logic;
+	using NUnit.Framework;
+
+	public class Day_13 : DayBase2022
+	{
+		public override int Day => 13;
+
+		private List<Packet> packets = new();
+
+		private bool testing = false;
+
+		[SetUp]
+		public async Task SetUp()
+		{
+			var input = testing
+				? TestInput.Split("\r\n").ToList()
+				: await this.SplitInput();
+
+			this.packets = input.Where(x => x != "").Select(ParsePacket).ToList();
+		}
+
+		[Test] // 6187
+		public override void PartOne()
+		{
+			var answer = 0;
+			for (var i = 0; i < packets.Count; i += 2)
+			{
+				var left = packets[i];
+				var right = packets[i + 1];
+
+				if (ComparePacketOrder(left, right)!.Value)
+				{
+					answer += ((i / 2) + 1);
+				}
+			}
+
+			answer.Pass();
+		}
+
+		[Test]
+		public override void PartTwo()
+		{
+			packets.AddRange(new List<Packet>
+			{
+				new Packet
+				{
+					Id = 2,
+					Packets = new Dictionary<int, Packet>
+					{
+						{ 0, new Packet { Integers = new Dictionary<int, int> { { 0, 2 } } } }
+					}
+				},
+				new Packet
+				{
+					Id = 6,
+					Packets = new Dictionary<int, Packet>
+					{
+						{ 0, new Packet { Integers = new Dictionary<int, int> { { 0, 6 } } } }
+					}
+				}
+			});
+
+			while (true)
+			{
+				var orderChanged = false;
+
+				for (var i = 1; i < this.packets.Count; i++)
+				{
+					if (ComparePacketOrder(this.packets[i - 1], this.packets[i]) == false)
+					{
+						(this.packets[i - 1], this.packets[i]) = (this.packets[i], this.packets[i - 1]);
+						orderChanged = true;
+					}
+				}
+
+				if (!orderChanged)
+				{
+					break;
+				}
+			}
+
+			var twoDividerIndex = this.packets.FindIndex(x => x.Id == 2) + 1;
+			var sixDividerIndex = this.packets.FindIndex(x => x.Id == 6) + 1;
+
+			(twoDividerIndex * sixDividerIndex).Pass();
+		}
+
+		private static Packet ParsePacket(string input)
+		{
+			var numbersRegex = new Regex("^([0-9]|,)+");
+
+			input = input.Substring(1, input.Length - 2);
+			var packet = new Packet();
+
+			while (input != "")
+			{
+				foreach (var number in numbersRegex.Matches(input).SelectMany(x => x.Value.Split(",")).Where(x => !string.IsNullOrEmpty(x)).Select(x => x.ToInt()))
+				{
+					packet.Integers.Add(packet.NextKey, number);
+				}
+
+				var nestedPacketStartIndex = input.IndexOf("[");
+				if (nestedPacketStartIndex >= 0)
+				{
+					input = input.Substring(nestedPacketStartIndex, input.Length - nestedPacketStartIndex);
+
+					var tracker = 0;
+					var nestedPacketEndIndex = input
+						.ToCharArray().ToList()
+						.FindIndex(c =>
+						{
+							tracker += c == '[' ? 1 : 0;
+							tracker -= c == ']' ? 1 : 0;
+
+							return tracker == 0;
+						});
+
+					packet.Packets.Add(packet.NextKey, ParsePacket(input.Substring(0, nestedPacketEndIndex + 1)));
+					input = input.Substring(nestedPacketEndIndex + 1, input.Length - nestedPacketEndIndex - 1);
+				}
+				else
+				{
+					return packet;
+				}
+			}
+
+			return packet;
+		}
+
+		private static bool? ComparePacketOrder(Packet left, Packet right)
+		{
+			var largest = left.NextKey > right.NextKey ? left : right;
+			for (var i = 0; i < largest.NextKey; i++)
+			{
+				if (left.NextKey <= i) { return true; }
+				else if (right.NextKey <= i) { return false; }
+				else if (left.IsInteger(i) && right.IsInteger(i))
+				{
+					if (left.Integers[i] < right.Integers[i]) { return true; }
+					else if (left.Integers[i] > right.Integers[i]) { return false; }
+				}
+				else if (left.IsPacket(i) && right.IsPacket(i))
+				{
+					var result = ComparePacketOrder(left.Packets[i], right.Packets[i]);
+					if (result.HasValue) { return result.Value; }
+				}
+				else
+				{
+					var leftPacket = left.IsPacket(i) ? left.Packets[i] : new Packet { Integers = new Dictionary<int, int>{ { 0, left.Integers[i] } }};
+					var rightPacket = right.IsPacket(i) ? right.Packets[i] : new Packet { Integers = new Dictionary<int, int>{ { 0, right.Integers[i] } }};
+
+					var result = ComparePacketOrder(leftPacket, rightPacket);
+					if (result.HasValue) { return result.Value; }
+				}
+			}
+
+			return null;
+		}
+
+		private class Packet
+		{
+			internal int Id { get; set; }
+
+			internal Dictionary<int, int> Integers { get; set; } = new ();
+			internal Dictionary<int, Packet> Packets { get; set; } = new();
+
+			internal int NextKey => Math.Max(Integers.Keys.LastOr(-1), Packets.Keys.LastOr(-1)) + 1;
+
+			internal bool IsInteger(int index)
+			{
+				return this.Integers.ContainsKey(index);
+			}
+
+			internal bool IsPacket(int index)
+			{
+				return this.Packets.ContainsKey(index);
+			}
+
+			public override string ToString()
+			{
+				var stringData = new List<string>();
+				for (var i = 0; i < this.NextKey; i++)
+				{
+					if (Integers.TryGetValue(i, out var integer))
+					{
+						stringData.Add(integer.ToString());
+					}
+					else if (Packets.TryGetValue(i, out var packet))
+					{
+						stringData.Add(packet.ToString());
+					}
+				}
+
+				return $"[{stringData.Join(",")}]";
+			}
+		}
+
+		private const string TestInput = @"[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
+	}
+
+	public static class Day13Extensions
+	{
+		public static T LastOr<T>(this IEnumerable<T> input, T defaultValue)
+		{
+			return input.Any() ? input.Last() : defaultValue;
+		}
+	}
+}
---
+++ AdventOfCode/PuzzleSolvers/2022/Day_14.cs
@@ -0,0 +1,265 @@
+﻿namespace AdventOfCode.PuzzleSolvers._2022
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Logic;
+	using NUnit.Framework;
+
+	public class Day_14 : DayBase2022
+	{
+		public override int Day => 14;
+
+		private List<List<Node>> grid = new ();
+		private Dictionary<int, Node> nodes = new ();
+
+		private int gridXStart;
+		private static int width, height, leftStack, rightStack;
+
+		[SetUp]
+		public async Task SetUp()
+		{
+			var input = await this.SplitInput();
+
+			var formattedLines = input.Select(line => (List<(int posX, int posY)>)line.Split(" -> ").Select(x => (x.Split(",")[0].ToInt(), x.Split(",")[1].ToInt())).ToList()).ToList();
+
+			var minX = formattedLines.SelectMany(x => x.Select(y => y.posX)).Min();
+			var maxX = formattedLines.SelectMany(x => x.Select(y => y.posX)).Max();
+			
+			var maxY = formattedLines.SelectMany(x => x.Select(y => y.posY)).Max();
+
+			gridXStart = minX;
+			width = maxX - minX + 1;
+			height = maxY + 1;
+
+			for (var y = 0; y < height; y++)
+			{
+				grid.Add(new List<Node>());
+
+				for (var x = 0; x < width; x++)
+				{
+					var node = new Node
+					{
+						Id = (y * width) + x
+					};
+
+					grid[y].Add(node);
+					nodes.Add(node.Id, node);
+				}
+			}
+
+			foreach (var line in formattedLines)
+			{
+				for (var i = 1; i < line.Count; i++)
+				{
+					var (from, to) = (line[i - 1], line[i]);
+
+					if (from.posX != to.posX)
+					{
+						var (smallest, largest) = (Math.Min(from.posX, to.posX), Math.Max(from.posX, to.posX));
+						for (var j = 0; j <= largest - smallest; j++)
+						{
+							grid[from.posY][smallest - gridXStart + j].IsRock = true;
+						}
+					}
+					else
+					{
+						var (smallest, largest) = (Math.Min(from.posY, to.posY), Math.Max(from.posY, to.posY));
+						for (var j = 0; j <= largest - smallest; j++)
+						{
+							grid[smallest + j][from.posX - gridXStart].IsRock = true;
+						}
+					}
+				}
+			}
+
+			grid[0][500 - gridXStart].IsStart = true;
+		}
+
+		[Test]
+		public override void PartOne()
+		{
+			var startingNodeId = this.grid.SelectMany(x => x).Single(x => x.IsStart).Id;
+			Node trackingNode = null;
+
+			while (true)
+			{
+				trackingNode ??= this.nodes[startingNodeId];
+
+				var (target, final, _) = NodeFallTarget(trackingNode, false);
+				if (target == null)
+				{
+					break;
+				}
+
+				if (final)
+				{
+					trackingNode.IsSand = true;
+					trackingNode = null;
+				}
+				else
+				{
+					trackingNode = target;
+				}
+			}
+
+			DrawGrid();
+		}
+
+		[Test]
+		public override void PartTwo()
+		{
+			for (var i = 0; i < grid.Count; i++)
+			{
+				grid[i] = new List<List<Node>> { new() { new Node() }, grid[i], new() { new Node() } }.SelectMany(x => x).ToList();
+			}
+
+			width += 2;
+			grid.Add(new List<Node>());
+			grid.Add(new List<Node>());
+
+			for (var i = 0; i < width; i++)
+			{
+				grid[height].Add(new Node());
+				grid[height + 1].Add(new Node { IsRock = true });
+			}
+
+			height += 2;
+
+			for (var y = 0; y < height; y++)
+			{
+				for (var x = 0; x < width; x++)
+				{
+					grid[y][x].Id = (y * width) + x;
+				}
+			}
+
+			this.nodes.Clear();
+			this.grid.SelectMany(x => x).ToList().ForEach(x => this.nodes.Add(x.Id, x));
+
+			var startIndex = this.grid.SelectMany(x => x).Single(x => x.IsStart).Id;
+			Node trackingNode = null;
+
+			while (true)
+			{
+				trackingNode ??= nodes[startIndex];
+				var (target, final, direction) = NodeFallTarget(trackingNode, true);
+
+				if (target == null)
+				{
+					var stackSize = height - trackingNode.PosY - 1;
+					var hill = direction == StackDirection.Left ? leftStack : rightStack;
+					if (hill < HillSize(stackSize - 1))
+					{
+						leftStack += direction == StackDirection.Left ? 1 : 0;
+						rightStack += direction == StackDirection.Left ? 0 : 1;
+
+						continue;
+					}
+
+					trackingNode.IsSand = true;
+					trackingNode = null;
+
+					continue;
+				}
+
+				if (final)
+				{
+					trackingNode.IsSand = true;
+					if (trackingNode.IsStart)
+					{
+						break;
+					}
+
+					trackingNode = null;
+					continue;
+				}
+
+				trackingNode = target;
+			}
+
+			DrawGrid();
+		}
+
+		private (Node target, bool finalNode, StackDirection? direction) NodeFallTarget(Node current, bool useSideStack)
+		{
+			if (nodes.TryGetValue(current.Id + width, out var nodeBelow))
+			{
+				if (nodeBelow.IsFree)
+				{
+					return (nodeBelow, false, null);
+				}
+			}
+			else
+			{
+				return (null, false, null);
+			}
+
+			if (nodes.TryGetValue(current.Id + width - 1, out var nodeBelowLeft) &&
+			    nodeBelowLeft.PosY == current.PosY + 1)
+			{
+				if (nodeBelowLeft.IsFree)
+				{
+					return (nodeBelowLeft, false, null);
+				}
+			}
+			else if (useSideStack)
+			{
+				var stackLeftMax = HillSize(height - current.PosY - 2);
+				if (stackLeftMax > leftStack)
+				{
+					return (null, false, StackDirection.Left);
+				}
+			}
+			else
+			{
+				return (null, false, StackDirection.Left);
+			}
+
+			if (nodes.TryGetValue(current.Id + width + 1, out var nodeBelowRight) &&
+			    nodeBelowRight.PosY == current.PosY + 1)
+			{
+				if (nodeBelowRight.IsFree)
+				{
+					return (nodeBelowRight, false, null);
+				}
+			}
+			else
+			{
+				return (null, false, StackDirection.Right);
+			}
+
+			return (current, true, null);
+		}
+
+		private void DrawGrid()
+		{
+			var drawnGrid = grid.Select(x => x.Select(x => x.IsStart ? "S" : x.IsRock ? "#" : x.IsSand ? "0" : ".").Join()).Join("\n");
+			$"{grid.SelectMany(x => x).Count(x => x.IsSand) + leftStack + rightStack} \n\n{drawnGrid}".Pass();
+		}
+
+		private static int HillSize(int stackSize)
+		{
+			return stackSize == 0 ? 0 : stackSize + HillSize(stackSize - 1);
+		}
+
+		private class Node
+		{
+			internal int Id { get; set; }
+			internal bool IsRock { get; set; }
+			internal bool IsStart { get; set; }
+			internal bool IsSand { get; set; }
+
+			internal bool IsFree => !IsRock && !IsSand;
+			internal int PosX => this.Id % width;
+			internal int PosY => this.Id / width;
+		}
+
+		private enum StackDirection
+		{
+			Left = 0,
+			Right = 1
+		}
+	}
+}
